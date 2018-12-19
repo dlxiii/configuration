c77e935eef295a5c8e3abacfb479109068d65335
Yulong WANG <32951329+dlxiii@users.noreply.github.com>
2018-12-20 00:45:34 +0900

    Update config_anaconda.sh

--- config_anaconda.sh
+++ config_anaconda.sh
@@ -4,10 +4,10 @@ export VERSION=5.3.1
 export DATETIME=$(date '+%Y-%m-%d %H:%M:%S')
 export PREFIX=/home/usr0/n70110d/usr/local/anaconda3
 
-#wget -P /home/usr0/n70110d/downloads https://repo.anaconda.com/archive/Anaconda3-$VERSION-Linux-x86_64.sh
+wget -P /home/usr0/n70110d/downloads https://repo.anaconda.com/archive/Anaconda3-$VERSION-Linux-x86_64.sh
 
 cd /home/usr0/n70110d/downloads
-#mkdir -p '${PREFIX}'
+mkdir -p '${PREFIX}'
 bash ./Anaconda3-$VERSION-Linux-x86_64.sh -b -p ${PREFIX}/$VERSION
 
 echo '# '$DATETIME': install the anaconda3, the path is:'>> ~/.bashrc
